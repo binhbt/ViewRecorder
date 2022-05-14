546db7de29dc861b3c5a8c56bd38af771d53ed4b
binhbt <thanhbinh.gd@gmail.com>
2022-05-14 20:27:21 +0700

    init

---
+++ app/build/generated/source/buildConfig/debug/com/z4hyoung/demo/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.z4hyoung.demo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.z4hyoung.viewrecorderdemo";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
